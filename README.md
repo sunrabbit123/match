74612e4c1683e9ecf74e69896a0d988e7231aed5
sunrabbit123 <qudwls185@naver.com>
2024-10-28 23:19:39 +0900

    initial commit
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

---
+++ README.md
@@ -0,0 +1,17 @@
+# @sunrabbit123/match
+
+I want a better match.
+
+The code piece is simple, so you can copy and paste it, or you can install it.
+
+However, if you copy and paste, please press the star once.
+
+## Installation
+
+```bash
+npm install @sunrabbit123/match
+# or
+yarn add @sunrabbit123/match
+```
+
+## Usage
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+type Matcher<T, R> = {
+    case: <U extends T, V>(typeguard: <U extends T>(target: T) => target is U, map: (guarded: U) => V) => Matcher<T, R | V>,
+    get: () => R,
+};
+
+type CaseStorage<T, U extends T> = {
+    typeGuard: (target: T) => target is U,
+    map: (guarded: U) => unknown,
+}[];
+
+const getMatcher = <T>(target: T, storage: CaseStorage<T>): Matcher<T, undefined> => ({
+    case: <U extends T, V>(typeGuard: (target: T) => target is U, map: (guarded: U) => V) => {
+        storage.push({typeGuard, map});
+        return getMatcher(target, storage);
+    },
+    get: () => {
+        const stored = storage.find(v => v.typeGuard(target));
+        if(stored === undefined) {
+            return undefined;
+        }
+        return stored.map(target);
+    }
+});
+export const match = <T>(target: T) => {
+    return getMatcher(target, []);
+};

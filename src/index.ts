cd2b0df9eafc30227c4456749c96cd05af0cad1e
sunrabbit123 <qudwls185@naver.com>
2024-10-29 14:48:06 +0900

    fix: ambiogus types
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/ban-types */
-export const match = <T>(target: T) => {
-  type MatcherArgument<U extends T, V> = { typeGuard: (target: T) => target is U; map: (guarded: U) => V };
+type MatcherArgument<T, U extends T, V> = { typeGuard: (target: T) => target is U; map: (guarded: U) => V };
 
-  const getMatcher = <U extends { typeGuard: Function; map: Function }, R>(storage: Array<U>) => ({
-    case: <V extends T, W>(fn: MatcherArgument<V, W>) => {
-      const redeclareStorage: Array<(typeof storage)[0] | MatcherArgument<V, W>> = storage;
+export type Matcher<T, U extends { typeGuard: Function; map: Function }, R> = {
+  case: <V extends T, W>(fn: MatcherArgument<T, V, W>) => Matcher<T, U | MatcherArgument<T, V, W>, R | W>;
+  get: () => R | undefined;
+};
+
+export const match = <T>(target: T) => {
+  const getMatcher = <U extends { typeGuard: Function; map: Function }, R>(storage: Array<U>): Matcher<T, U, R> => ({
+    case: <V extends T, W>(fn: MatcherArgument<T, V, W>) => {
+      const redeclareStorage: Array<(typeof storage)[0] | MatcherArgument<T, V, W>> = storage;
       redeclareStorage.push(fn);
-      return getMatcher<U | MatcherArgument<V, W>, R | W>(redeclareStorage);
+      return getMatcher<U | MatcherArgument<T, V, W>, R | W>(redeclareStorage);
     },
     get: (): R | undefined => {
       const stored = storage.find((v) => v.typeGuard(target));

70b215da908fd652eb26b6f7eb5fa141de1409cc
sunrabbit123 <qudwls185@naver.com>
2024-10-29 11:10:35 +0900

    chore: formmat
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
 type Matcher<T, R> = {
-    case: <U extends T, V>(typeguard: <U extends T>(target: T) => target is U, map: (guarded: U) => V) => Matcher<T, R | V>,
-    get: () => R,
+  case: <U extends T, V>(
+    typeguard: <U extends T>(target: T) => target is U,
+    map: (guarded: U) => V
+  ) => Matcher<T, R | V>;
+  get: () => R;
 };
 
 type CaseStorage<T, U extends T> = {
-    typeGuard: (target: T) => target is U,
-    map: (guarded: U) => unknown,
+  typeGuard: (target: T) => target is U;
+  map: (guarded: U) => unknown;
 }[];
 
 const getMatcher = <T>(target: T, storage: CaseStorage<T>): Matcher<T, undefined> => ({
-    case: <U extends T, V>(typeGuard: (target: T) => target is U, map: (guarded: U) => V) => {
-        storage.push({typeGuard, map});
-        return getMatcher(target, storage);
-    },
-    get: () => {
-        const stored = storage.find(v => v.typeGuard(target));
-        if(stored === undefined) {
-            return undefined;
-        }
-        return stored.map(target);
+  case: <U extends T, V>(typeGuard: (target: T) => target is U, map: (guarded: U) => V) => {
+    storage.push({ typeGuard, map });
+    return getMatcher(target, storage);
+  },
+  get: () => {
+    const stored = storage.find((v) => v.typeGuard(target));
+    if (stored === undefined) {
+      return undefined;
     }
+    return stored.map(target);
+  },
 });
 export const match = <T>(target: T) => {
-    return getMatcher(target, []);
+  return getMatcher(target, []);
 };
